a77eee1375fa88a68eb3031af86cf8d1f17c06b0
Kai_EV <kasicer1@gmail.com>
2023-03-14 17:15:59 +0100

    add API_KEY

--- app/src/main/java/com/example/ue_proyectointegrador/dao/PeliculasDao.java
+++ app/src/main/java/com/example/ue_proyectointegrador/dao/PeliculasDao.java
@@ -1,10 +1,10 @@
 package com.example.ue_proyectointegrador.dao;
 
+import androidx.room.Dao;
 import androidx.room.Query;
-
+@Dao
 public interface PeliculasDao {
 
-    //Pasamos el nombre de la pelicula y nos devuelve el Cine
-    //@Query("SELECT ")
+
 
 }
